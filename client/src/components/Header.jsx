0fb803952ecaffc8be7a9e4c5ef555c66645fc40
mynameist04st3r <t04st3r@mac.com>
2025-04-01 18:19:24 -0400

    Incorporated app color scheme. Added header component.

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './Header.css'
+
+function Header() {
+
+  return (
+    <>
+      <div className="header-component-container">
+        <h1>What CAN I make?</h1>
+      </div>
+    </>
+  )
+}
+
+export default Header
